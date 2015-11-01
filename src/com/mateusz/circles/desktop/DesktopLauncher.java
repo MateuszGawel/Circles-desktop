ba2a95beb5783ec93f512d14fd9bf58c22709085
Mateusz Gaweł <mg.mateuszgawel@gmail.com>
2015-11-01 21:11:28 +0100

    initial desktop commit

---
+++ src/com/mateusz/circles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mateusz.circles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mateusz.circles.Circles;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Circles(), config);
+	}
+}
